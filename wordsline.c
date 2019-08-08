6b9932ed57f3a66aa1b0178292c305e4dd8a74dd
shalinikalaiselvi <48079373+shalinikalaiselvi@users.noreply.github.com>
2019-08-08 21:48:06 +0530

    Update wordsline.c

--- wordsline.c
+++ wordsline.c
@@ -7,9 +7,7 @@ void main()
     printf("enter the string\n");
     scanf("%[^\n]s", s);
     for (i = 0;s[i] != '\0';i++)
-    {
-        if (s[i] == ' ')
-            count++;    
-    }
+    {if (s[i] == ' ')
+            count++;    }
     printf("number of words in given string are: %d\n", count + 1);
 }

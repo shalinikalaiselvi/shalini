a55ebc25836e1dc9a5bb11a961f758ee95aa8e58
shalini <48079373+shalinikalaiselvi@users.noreply.github.com>
2019-08-14 20:02:51 +0530

    Create arithmeticprogression.java

---
+++ arithmeticprogression.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+import java.math.*;
+import java.lang.*;
+public class ap
+{
+     public static void main(String []args)
+     {
+        Scanner in = new Scanner(System.in);
+        double AP;
+        int a,n,d;
+        a=3; d=8; n=4;
+        AP=a+(n-1)*d;
+        System.out.println("Arithmetic progression is :"+AP);
+     }
+}
